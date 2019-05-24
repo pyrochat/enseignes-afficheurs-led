7eb020d4f347e304f9644aa1bd2ef5b6b9099a3b
Pierre-Yves Rochat <pyr@pyr.ch>
2019-05-24 22:41:45 +0200

    Test 2 chapitres c

---
+++ manuscript/resources/electric-images/gen-images.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ##########
+#
+# Génération des images bitmaps à partir des images SVG
+# Usage : ./gen-images.sh NUMERO-CHAPITRE
+#
+# Ce script ne fonctionne pas sur OSX (malgré mes tentatives).
+#
+# Mars 2016, Nicolas Jeanmonod
+#
+# ##########
+
+SCRIPT=`realpath $0`
+CUR_DIR=`dirname $SCRIPT`
+echo $CUR_DIR
+
+if [[ "$OSTYPE" == darwin14 ]]; then
+	shopt -s expand_aliases
+	alias inkscape="/Applications/inkscape.app/Contents/Resources/bin/inkscape"
+fi
+
+inkscape -z -C atome.svg -e=atome.png -d=300
+convert -background white -flatten atome.png atome.jpg
+
+inkscape -z -C pile-led.svg -e=pile-led.png -d=300
+convert -background white -flatten pile-led.png pile-led.jpg
+exit 0
